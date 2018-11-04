c6f68d4f0ef87b53f704977fe752ff3c4d748449
burton1234 <bill.burton@gcinc.com>
2018-11-03 20:21:06 -0700

    7

--- assignments/lesson-7/scripts/windchill.js
+++ assignments/lesson-7/scripts/windchill.js
@@ -1,9 +1,5 @@
 
-                let x = parseInt(document.getElementById("value1").innerHTML);
-                let y = parseInt(document.getElementById("value2").innerHTML);
+         <script>
+             
                 
-                let result = 35.74 + (0.6215 * x) + (0.4275 * x - 35.75)  *  y ^ 0.16;
-                
-                // Output - round to one decimal and output to div
-                document.getElementById("output").innerHTML =
-                 "The Wind Chill Is <strong>" + result.toFixed(1) + "</strong>.";
+</script>
