32a2bdf27fd208074d50cfb166014dfed798093c
Burton <Bill.Burton@gcinc.com>
2018-11-24 14:44:22 -0800

    10

--- assignments/lesson-10_1/weatherapi.js
+++ assignments/lesson-10_1/weatherapi.js
@@ -1,6 +1,6 @@
 
 let weatherRequest = new XMLHttpRequest();
- let urlAPI = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&unit=imperial&APPID=467917115c2fb1ab165e7743f138efd0";
+ let urlAPI = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=467917115c2fb1ab165e7743f138efd0";
  weatherRequest.open('GET',urlAPI, true);
  weatherRequest.send();
  weatherRequest.onload = function() {
@@ -8,12 +8,9 @@ let weatherRequest = new XMLHttpRequest();
    (weatherRequest.responseText);
    console.log(weatherData);
    //using main instead for array
-   document.getElementById("current_temp")
-   .innerHTML = weatherData.main.temp;
-   document.getElementById("humidity")
-   .innerHTML = weatherData.main.humidity;
-   document.getElementById("wind_spd")
-   .innerHTML = weatherData.main.speed;
+   document.getElementById("current_temp").innerHTML = weatherData.main.temp;
+   document.getElementById("humidity").innerHTML = weatherData.main.humidity;
+   document.getElementById("wind_spd").innerHTML = weatherData.wind.speed;
    document.getElementById("wind_dir").innerHTML = weatherData.wind.deg;
 let imagescrc = "http://openweathermap.org/img/w" + weatherData.weather[0].icon + ".png";
 document.getElementById('weatherimage')
