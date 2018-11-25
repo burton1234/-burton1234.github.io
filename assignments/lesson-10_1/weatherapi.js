caca36edebb5d46312eb0761abe8baedfca2be90
Burton <Bill.Burton@gcinc.com>
2018-11-24 18:51:45 -0800

    10

--- assignments/lesson-10_1/weatherapi.js
+++ assignments/lesson-10_1/weatherapi.js
@@ -7,7 +7,6 @@ let weatherRequest = new XMLHttpRequest();
    let weatherData = JSON.parse
    (weatherRequest.responseText);
    console.log(weatherData);
-   //using main instead for array
    document.getElementById("current_temp").innerHTML = weatherData.main.temp;
    document.getElementById("description").innerHTML = weatherData.weather[0].description;
    document.getElementById("humidity").innerHTML = weatherData.main.humidity;
@@ -17,4 +16,3 @@ let imagescrc = "http://openweathermap.org/img/w" + weatherData.weather[0].icon 
 document.getElementById('weatherimage')
 .innerHTML = imagesrc;   
 }
-//end of onload function
