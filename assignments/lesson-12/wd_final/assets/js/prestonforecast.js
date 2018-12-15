4208516c60f8ef3b34d1aea018d33f77835ef25c
burton1234 <bill.burton@gcinc.com>
2018-12-15 15:25:25 -0800

    12

---
+++ assignments/lesson-12/wd_final/assets/js/prestonforecast.js
@@ -0,0 +1,21 @@
+fetch('https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=467917115c2fb1ab165e7743f138efd0')
+.then(function(response) {
+    return response.json();
+})
+.then(function(jsObject) {
+    console.log(jsObject);
+
+    let tempforecast = [];
+    let day = 1;
+    jsObject.list.forEach(x => {
+        if (x.dt_txt.includes('15:00:00')) {
+            tempforecast[day] = x.main.temp;
+            day++;
+        }
+    });
+
+    for (var i = 1; i <= 5; i++) {
+        document.getElementById('forecastday' + i)
+        .innerHTML = tempforecast[i];
+    }
+});
